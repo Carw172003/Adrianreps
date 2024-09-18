895eb0e782e37012a42ce9074b99c968076b89f7
AdrianRW17 <adrian16rw03@gmail.com>
2024-09-18 10:00:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// 9.1 Comentarios : este es un comentario de una sola linea
+// 9.2 ESCRITURA EN JAVASCRIPT
+console.log("Ejemplos de Javascript estan en la consola")
+
+
+
+
+// .3 Sentencias
+let x = 10; // esta es una sentencia que asigna el valor 10 a x
+let y = 5;  // otra sentencia
+console.log(x + y);
+
+
+//  9.5 Operadores Aritmeticos y Expresiones
+let suma= x + y; //suma
+let resta= x- y; // resta
+let multiplicacion= x* y; // Multiplicacion
+let division = x/ y; // division 
+console.log("suma:",suma)
+console.log("resta:",resta)
+console.log("multiplicacion:",multiplicacion)
+console.log("division:",division);
+
+
+//  9.7 Javascript es Case Sensitive
+let nombre= "Juan"// 'nombre' es diferente a ' Nombre'
+let Nombre= "Pedro" // Identificadores distintos
+console.log("Nombre en minuscula:", nombre)
+console.log("Nombre en mayuscula:"  Nombre)
+
+
+// 9.8 Tipos de Datos
+let esVerdadero= true; // Booleano
+let numero = 100;  // Numero
+let texto = "Hola,mundo"; // String
+console.log("Boleano",esVerdadero)
+console.log("Numero", numero)
+console.log("String", texto)
+
+// 9.10 Numeros 
+let numeroEntero  = 9;  // Numero entero
+let numeroDecimal = 10.3;  // Numero decimal
+console.log("Numero entero",numeroEntero);
+console.log("Numero decimal", numeroDecimal);
+console.log(numerodecimal*numeroEntero);
